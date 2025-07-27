d76f45c379b40f127d4ca0ec1ff4a8e7b85c45cd
mayurpawar17 <mayurpawar4908@gmail.com>
2025-07-27 17:12:01 +0530

    string interpliation practice

---
+++ variables/string_interpoliation_practice.dart
@@ -0,0 +1,10 @@
+void main() {
+  double temperature = 20;
+  int value = 3;
+  String pizza = 'pizza';
+  String pasta = 'pasta';
+
+  print("The temperature is ${temperature}C");
+  print("$value plus $value make ${value + value}");
+  print("I like $pizza and $pasta");
+}
